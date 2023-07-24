1150565b303e4cccb4199edb396bd3f2c7cd1771
kts of kettek <kts@kettek.net>
2023-07-24 16:57:44 -0700

    Add attribute colors and descriptions

--- game/attribute.go
+++ game/attribute.go
@@ -1,5 +1,7 @@
 package game
 
+import "image/color"
+
 type Attribute uint8
 
 const (
@@ -9,4 +11,22 @@ const (
 	AttributeFunk
 )
 
+const (
+	AttributeSwoleDescription  = "Swole determines damage and health"
+	AttributeZoomsDescription  = "Zooms determines speed and dodge"
+	AttributeBrainsDescription = "Brains determines spell damage and ability to by-pass traps"
+	AttributeFunkDescription   = "Funk determines luck and area of effect bonuses"
+)
+
 type AttributeLevel float64
+
+var (
+	ColorSwole         = color.NRGBA{128, 32, 32, 255}
+	ColorSwoleVibrant  = color.NRGBA{200, 100, 100, 255}
+	ColorZooms         = color.NRGBA{128, 128, 32, 255}
+	ColorZoomsVibrant  = color.NRGBA{200, 200, 100, 255}
+	ColorBrains        = color.NRGBA{32, 32, 128, 255}
+	ColorBrainsVibrant = color.NRGBA{100, 100, 200, 255}
+	ColorFunk          = color.NRGBA{128, 32, 128, 255}
+	ColorFunkVibrant   = color.NRGBA{200, 100, 200, 255}
+)
