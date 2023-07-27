0df6992bee8d65f77b35859c2db06d53ded9f6fc
kts of kettek <kts@kettek.net>
2023-07-26 21:10:12 -0700

    Use cached grid image

--- client/states/game/grid.go
+++ client/states/game/grid.go
@@ -3,6 +3,7 @@ package game
 import (
 	"image/color"
 
+	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	"github.com/kettek/morogue/client/ifs"
 )
@@ -12,6 +13,22 @@ type Grid struct {
 	width, height         int
 	cellWidth, cellHeight int
 	color                 color.Color
+	image                 *ebiten.Image
+}
+
+func (grid *Grid) makeImage() {
+	if grid.width == 0 || grid.height == 0 {
+		return
+	}
+	grid.image = ebiten.NewImage(grid.width, grid.height)
+	cx := grid.width / grid.cellWidth
+	cy := grid.height / grid.cellHeight
+	for x := 1; x != cx; x++ {
+		vector.StrokeLine(grid.image, float32(x*grid.cellWidth), 0, float32(x*grid.cellWidth), float32(grid.height), 1, grid.color, false)
+	}
+	for y := 1; y != cy; y++ {
+		vector.StrokeLine(grid.image, 0, float32(y*grid.cellHeight), float32(grid.width), float32(y*grid.cellHeight), 1, grid.color, false)
+	}
 }
 
 func (grid *Grid) Size() (int, int) {
@@ -21,6 +38,7 @@ func (grid *Grid) Size() (int, int) {
 func (grid *Grid) SetSize(w, h int) {
 	grid.width = w
 	grid.height = h
+	grid.makeImage()
 }
 
 func (grid *Grid) CellSize() (int, int) {
@@ -30,6 +48,7 @@ func (grid *Grid) CellSize() (int, int) {
 func (grid *Grid) SetCellSize(w, h int) {
 	grid.cellWidth = w
 	grid.cellHeight = h
+	grid.makeImage()
 }
 
 func (grid *Grid) Offset() (x, y int) {
@@ -50,12 +69,10 @@ func (grid *Grid) SetColor(c color.Color) {
 }
 
 func (grid *Grid) Draw(ctx ifs.DrawContext) {
-	cx := grid.width / grid.cellWidth
-	cy := grid.height / grid.cellHeight
-	for x := 1; x != cx; x++ {
-		vector.StrokeLine(ctx.Screen, float32(x*grid.cellWidth+grid.offsetX), float32(grid.offsetY), float32(x*grid.cellWidth+grid.offsetX), float32(grid.height+grid.offsetY), 1, grid.color, false)
+	if grid.image == nil {
+		return
 	}
-	for y := 1; y != cy; y++ {
-		vector.StrokeLine(ctx.Screen, float32(grid.offsetX), float32(y*grid.cellHeight+grid.offsetY), float32(grid.width+grid.offsetX), float32(y*grid.cellHeight+grid.offsetY), 1, grid.color, false)
-	}
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(float64(grid.offsetX), float64(grid.offsetY))
+	ctx.Screen.DrawImage(grid.image, &opts)
 }
