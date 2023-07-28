a03fe7ea525bdd81ba57597e197a6c64a9e94487
kts of kettek <kts@kettek.net>
2023-07-28 15:20:31 -0700

    Add Save/Restore and DrawWithOutline

--- client/ifs/text.go
+++ client/ifs/text.go
@@ -1,13 +1,74 @@
 package ifs
 
 import (
+	"image/color"
+
 	"github.com/tinne26/etxt"
+	"golang.org/x/image/font/sfnt"
 )
 
 type TextRenderer struct {
 	etxt.Renderer
+
+	outlineColor color.Color
+	//
+	savedFont         *sfnt.Font
+	savedColor        color.Color
+	savedSize         float64
+	savedScale        float64
+	savedBlendMode    etxt.BlendMode
+	savedAlign        etxt.Align
+	savedOutlineColor color.Color
 }
 
 func NewTextRenderer(r *etxt.Renderer) *TextRenderer {
-	return &TextRenderer{*r}
+	return &TextRenderer{
+		Renderer: *r,
+	}
+}
+
+func (t *TextRenderer) DrawWithOutline(target etxt.TargetImage, text string, x, y int) {
+	c := t.GetColor()
+	t.SetColor(t.outlineColor)
+	{
+		t.Draw(target, text, x-1, y)
+		t.Draw(target, text, x+1, y)
+		t.Draw(target, text, x, y-1)
+		t.Draw(target, text, x, y+1)
+
+		t.Draw(target, text, x-1, y+1)
+		t.Draw(target, text, x+1, y-1)
+		t.Draw(target, text, x-1, y-1)
+		t.Draw(target, text, x+1, y+1)
+	}
+	t.SetColor(c)
+	t.Draw(target, text, x, y)
+}
+
+func (t *TextRenderer) SetOutlineColor(c color.Color) {
+	t.outlineColor = c
+}
+
+func (t *TextRenderer) GetOutlineColor() color.Color {
+	return t.outlineColor
+}
+
+func (t *TextRenderer) Save() {
+	t.savedFont = t.GetFont()
+	t.savedColor = t.GetColor()
+	t.savedSize = t.GetSize()
+	t.savedScale = t.GetScale()
+	t.savedBlendMode = t.GetBlendMode()
+	t.savedAlign = t.GetAlign()
+	t.savedOutlineColor = t.GetOutlineColor()
+}
+
+func (t *TextRenderer) Restore() {
+	t.SetFont(t.savedFont)
+	t.SetColor(t.savedColor)
+	t.SetSize(t.savedSize)
+	t.SetScale(t.savedScale)
+	t.SetBlendMode(t.savedBlendMode)
+	t.SetAlign(t.savedAlign)
+	t.SetOutlineColor(t.outlineColor)
 }
