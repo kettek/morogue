fa07a40d37a283fd27162de3495acf4660406d96
kts of kettek <kts@kettek.net>
2023-08-04 04:49:34 -0700

    Bring weapon up to armor in features

--- client/states/game/objectinfo.go
+++ client/states/game/objectinfo.go
@@ -16,9 +16,42 @@ func addObjectInfo(ctx ifs.RunContext, object game.Object, arch game.Archetype, 
 	mediumColor := color.NRGBA{R: 200, G: 200, B: 100, A: 255}
 	heavyColor := color.NRGBA{R: 200, G: 100, B: 100, A: 255}
 
+	unarmedColor := color.NRGBA{R: 250, G: 150, B: 50, A: 255}
+	rangedColor := color.NRGBA{R: 50, G: 250, B: 50, A: 255}
+	meleeColor := color.NRGBA{R: 250, G: 50, B: 50, A: 255}
+
 	switch a := arch.(type) {
 	case game.WeaponArchetype:
-		// TODO
+		weaponColor := noneColor
+		if a.WeaponType == game.WeaponTypeRange {
+			weaponColor = rangedColor
+		} else if a.WeaponType == game.WeaponTypeMelee {
+			weaponColor = meleeColor
+		} else if a.WeaponType == game.WeaponTypeUnarmed {
+			weaponColor = unarmedColor
+		}
+
+		title := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s", a.Title), ctx.UI.BodyCopyFace, color.White))
+		values := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s %s", a.RangeString(), a.WeaponType), ctx.UI.BodyCopyFace, weaponColor))
+		desc := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(a.Description, ctx.UI.BodyCopyFace, color.NRGBA{128, 128, 128, 255}))
+
+		weaponLine := widget.NewContainer(
+			widget.ContainerOpts.Layout(widget.NewRowLayout(widget.RowLayoutOpts.Direction(widget.DirectionHorizontal))),
+		)
+		graphic := widget.NewGraphic(
+			widget.GraphicOpts.Image(embed.IconOffense),
+			widget.GraphicOpts.WidgetOpts(
+				widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+					Position: widget.RowLayoutPositionCenter,
+				}),
+			),
+		)
+		weaponLine.AddChild(values)
+		weaponLine.AddChild(graphic)
+
+		container.AddChild(title)
+		container.AddChild(weaponLine)
+		container.AddChild(desc)
 	case game.ArmorArchetype:
 		armorColor := noneColor
 		if a.ArmorType == game.ArmorTypeLight {
--- game/weapon.go
+++ game/weapon.go
@@ -1,25 +1,58 @@
 package game
 
-import "github.com/kettek/morogue/id"
+import (
+	"fmt"
+
+	"github.com/kettek/morogue/id"
+)
 
 // WeaponType is the type the weapon is considered as.
 type WeaponType uint8
 
 const (
-	WeaponTypeRange WeaponType = iota
-	WeaponTypeMelee
+	WeaponTypeMelee WeaponType = iota
+	WeaponTypeRange
 	WeaponTypeUnarmed
 )
+
+func (a WeaponType) String() string {
+	switch a {
+	case WeaponTypeRange:
+		return "range"
+	case WeaponTypeMelee:
+		return "melee"
+	case WeaponTypeUnarmed:
+		return "unarmed"
+	default:
+		return ""
+	}
+}
+
+func (a *WeaponType) UnmarshalJSON(b []byte) error {
+	switch string(b) {
+	case `"range"`:
+		*a = WeaponTypeRange
+	case `"melee"`:
+		*a = WeaponTypeMelee
+	case `"unarmed"`:
+		*a = WeaponTypeUnarmed
+	default:
+		*a = WeaponTypeRange
+	}
+	return nil
+}
 
 // WeaponArchetype is effectively a blueprint for a weapon.
 type WeaponArchetype struct {
 	ID                 id.UUID
-	Title              string    `msgpack:"T,omitempty"`
-	Image              string    `msgpack:"i,omitempty"`
-	PrimaryAttribute   Attribute `msgpack:"p,omitempty"` // Primary attribute to draw damage from.
-	SecondaryAttribute Attribute `msgpack:"s,omitempty"` // Secondary attribute to draw 50% damage from.
-	MinDamage          int       `msgpack:"m,omitempty"` // Character proficiency with a weapon increases min up to max.
-	MaxDamage          int       `msgpack:"M,omitempty"`
+	Title              string     `msgpack:"T,omitempty"`
+	Image              string     `msgpack:"i,omitempty"`
+	PrimaryAttribute   Attribute  `msgpack:"p,omitempty"` // Primary attribute to draw damage from.
+	SecondaryAttribute Attribute  `msgpack:"s,omitempty"` // Secondary attribute to draw 50% damage from.
+	Description        string     `msgpack:"d,omitempty"`
+	WeaponType         WeaponType `msgpack:"w,omitempty"`
+	MinDamage          int        `msgpack:"m,omitempty"` // Character proficiency with a weapon increases min up to max.
+	MaxDamage          int        `msgpack:"M,omitempty"`
 }
 
 func (a WeaponArchetype) Type() string {
@@ -28,6 +61,13 @@ func (a WeaponArchetype) Type() string {
 
 func (a WeaponArchetype) GetID() id.UUID {
 	return a.ID
+}
+
+func (a WeaponArchetype) RangeString() string {
+	if a.MinDamage == 0 {
+		return fmt.Sprintf("〜%d", a.MaxDamage)
+	}
+	return fmt.Sprintf("%d〜%d", a.MinDamage, a.MaxDamage)
 }
 
 // Weapon is a weapon.
