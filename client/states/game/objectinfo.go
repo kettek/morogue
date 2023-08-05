02d143482e6ecfb9906d4ae942012a427f151e3b
kts of kettek <kts@kettek.net>
2023-08-04 19:51:43 -0700

    Use TextArea for object description

--- client/states/game/objectinfo.go
+++ client/states/game/objectinfo.go
@@ -4,11 +4,55 @@ import (
 	"fmt"
 	"image/color"
 
+	eimage "github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 	"github.com/kettek/morogue/client/embed"
 	"github.com/kettek/morogue/client/ifs"
 	"github.com/kettek/morogue/game"
 )
+
+func makeDescription(ctx ifs.RunContext, txt string) *widget.TextArea {
+	return widget.NewTextArea(
+		widget.TextAreaOpts.ContainerOpts(
+			widget.ContainerOpts.WidgetOpts(
+				widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+					Position:  widget.RowLayoutPositionCenter,
+					MaxWidth:  400,
+					MaxHeight: 300,
+				}),
+				widget.WidgetOpts.MinSize(300, 30),
+			),
+		),
+		widget.TextAreaOpts.ControlWidgetSpacing(2),
+		widget.TextAreaOpts.ProcessBBCode(true),
+		widget.TextAreaOpts.FontColor(color.NRGBA{128, 128, 128, 255}),
+		widget.TextAreaOpts.FontFace(ctx.UI.BodyCopyFace),
+		widget.TextAreaOpts.Text(txt),
+		//This sets the background images for the scroll container
+		widget.TextAreaOpts.ScrollContainerOpts(
+			widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
+				Idle: ctx.UI.ItemInfoBackgroundImage,
+				Mask: ctx.UI.ItemInfoBackgroundImage,
+			}),
+		),
+		//This sets the eimages to use for the sliders
+		widget.TextAreaOpts.SliderOpts(
+			widget.SliderOpts.Images(
+				// Set the track eimages
+				&widget.SliderTrackImage{
+					Idle:  eimage.NewNineSliceColor(color.NRGBA{200, 200, 200, 255}),
+					Hover: eimage.NewNineSliceColor(color.NRGBA{200, 200, 200, 255}),
+				},
+				// Set the handle eimages
+				&widget.ButtonImage{
+					Idle:    eimage.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+					Hover:   eimage.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+					Pressed: eimage.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
+				},
+			),
+		),
+	)
+}
 
 func addObjectInfo(ctx ifs.RunContext, object game.Object, arch game.Archetype, container *widget.Container) {
 	noneColor := color.NRGBA{R: 200, G: 200, B: 200, A: 255}
@@ -33,7 +77,7 @@ func addObjectInfo(ctx ifs.RunContext, object game.Object, arch game.Archetype, 
 
 		title := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s", a.Title), ctx.UI.BodyCopyFace, color.White))
 		values := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s %s", a.RangeString(), a.WeaponType), ctx.UI.BodyCopyFace, weaponColor))
-		desc := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(a.Description, ctx.UI.BodyCopyFace, color.NRGBA{128, 128, 128, 255}))
+		desc := makeDescription(ctx, a.Description)
 
 		weaponLine := widget.NewContainer(
 			widget.ContainerOpts.Layout(widget.NewRowLayout(widget.RowLayoutOpts.Direction(widget.DirectionHorizontal))),
@@ -64,7 +108,7 @@ func addObjectInfo(ctx ifs.RunContext, object game.Object, arch game.Archetype, 
 
 		title := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s", a.Title), ctx.UI.BodyCopyFace, color.White))
 		values := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(fmt.Sprintf("%s %s", a.RangeString(), a.ArmorType), ctx.UI.BodyCopyFace, armorColor))
-		desc := widget.NewText(widget.TextOpts.ProcessBBCode(true), widget.TextOpts.Text(a.Description, ctx.UI.BodyCopyFace, color.NRGBA{128, 128, 128, 255}))
+		desc := makeDescription(ctx, a.Description)
 
 		armorLine := widget.NewContainer(
 			widget.ContainerOpts.Layout(widget.NewRowLayout(widget.RowLayoutOpts.Direction(widget.DirectionHorizontal))),
