da8b73e7320f13df5957ade3579fe2cd43880fcb
kts of kettek <kts@kettek.net>
2023-08-24 16:19:42 -0700

    Add width/height getter

--- gen/fixture.go
+++ gen/fixture.go
@@ -7,3 +7,16 @@ type Fixture struct {
 	Keys map[string]id.UUID
 	Rows []string
 }
+
+func (f Fixture) Width() (w int) {
+	for _, row := range f.Rows {
+		if len(row) > w {
+			w = len(row)
+		}
+	}
+	return
+}
+
+func (f Fixture) Height() int {
+	return len(f.Rows)
+}
