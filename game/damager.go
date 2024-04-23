3c3a10fdc01b62d08549045015b09df96e015721
kts of kettek <kts@kettek.net>
2024-04-23 15:26:34 -0700

    Adjust to use swole convenience func

--- game/damager.go
+++ game/damager.go
@@ -33,16 +33,17 @@ func (d *Damager) CalculateFromCharacter(c *Character) {
 		}
 	}
 	// FIXME: Don't assume Swole, use the weapon's preferred attribute.
+	swole := c.Swole()
 	var mainMin, mainMax, mainExtra, offMin, offMax, offExtra int
 	if mainHand != nil {
 		mainMin = mainHand.Archetype.(WeaponArchetype).MinDamage
 		mainMax = mainHand.Archetype.(WeaponArchetype).MaxDamage
-		if c.Archetype.(CharacterArchetype).Swole > AttributeLevel(mainMin) {
-			if c.Archetype.(CharacterArchetype).Swole > AttributeLevel(mainMax) {
+		if swole > AttributeLevel(mainMin) {
+			if swole > AttributeLevel(mainMax) {
 				mainMin = mainMax
-				mainExtra = (int(c.Archetype.(CharacterArchetype).Swole) - mainMax) / 2
+				mainExtra = (int(swole) - mainMax) / 2
 			} else {
-				mainMin = int(c.Archetype.(CharacterArchetype).Swole)
+				mainMin = int(swole)
 			}
 		}
 		d.Damages = append(d.Damages, Damage{
@@ -57,12 +58,12 @@ func (d *Damager) CalculateFromCharacter(c *Character) {
 	if offHand != nil {
 		offMin = offHand.Archetype.(WeaponArchetype).MinDamage
 		offMax = offHand.Archetype.(WeaponArchetype).MaxDamage
-		if c.Archetype.(CharacterArchetype).Swole > AttributeLevel(offMin) {
-			if c.Archetype.(CharacterArchetype).Swole > AttributeLevel(offMax) {
+		if swole > AttributeLevel(offMin) {
+			if swole > AttributeLevel(offMax) {
 				offMin = offMax
-				offExtra = (int(c.Archetype.(CharacterArchetype).Swole) - offMax) / 2
+				offExtra = (int(swole) - offMax) / 2
 			} else {
-				offMin = int(c.Archetype.(CharacterArchetype).Swole)
+				offMin = int(swole)
 			}
 		}
 		d.Damages = append(d.Damages, Damage{
@@ -78,7 +79,7 @@ func (d *Damager) CalculateFromCharacter(c *Character) {
 		d.Damages = append(d.Damages, Damage{
 			Source:  c.WID,
 			Min:     0,
-			Max:     int(c.Archetype.(CharacterArchetype).Swole) / 2,
+			Max:     int(swole) / 2,
 			Extra:   0,
 			Reduced: true,
 			Weapon:  WeaponTypeUnarmed,
